2e100bb4d171a113a9c4a3b6718682096361641d
VFDan <44413950+VFDan@users.noreply.github.com>
2020-04-12 19:10:45 -0400

    Resync Branches (#4)

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById('textarea').value = localStorage.getItem('content');
+document.getElementById('textarea').value = localStorage.getItem('content') || "";
+if (localStorage.getItem('dark') == null) {
+  localStorage.setItem('dark', false);
+}
+
+if (localStorage.getItem('dark') == 'true') {
+  document.body.className += ' ' + 'dark';
+} else {
+  document.body.className += ' ' + 'light';
+}
